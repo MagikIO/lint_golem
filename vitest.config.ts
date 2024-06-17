7c136a14df6c291a82c90d8c47332983e6d303b2
Abourass <Abourassa@AssetVal.com>
2024-06-17 11:37:15 -0600

    chore: Update vitest.config.ts to use v8 coverage provider

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      provider: 'v8',
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
